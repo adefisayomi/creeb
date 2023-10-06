919a5a7e4a61b2f0edee93f9604904c7c6c2f3cb
Dolapo oluwole adefisayomi <devbyclace@gmail.com>
2023-10-06 22:28:07 +0100

    updated file

---
+++ src/controllers/geePage.ts
@@ -0,0 +1,33 @@
+import puppeteer from 'puppeteer'
+
+
+
+export default async function getPage () {
+    try {
+        const browser = await puppeteer.launch({headless: 'new'});
+        const page = await browser.newPage()
+        await page.goto('https://example.com/');
+
+        const title = await page.$$eval('p', (doc) => {
+            const arr: any = []
+            doc.forEach(data => arr.push(data.textContent))
+            return arr
+        })
+        
+        await page.close()
+        await browser.close()
+        
+        return ({
+            success: true,
+            message: 'scraping complete',
+            data: title
+        })
+    }
+    catch(err: any) {
+        return ({
+            success: false,
+            message: err.message,
+            data: null
+        })
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,15 @@
 import express from 'express'
 import puppeteer from 'puppeteer'
+import getPage from './controllers/geePage'
+// import cookieParser from 'cookie-parser'
 
 const app = express()
 
+
 app.use('/', async (req, res) => {
 
-    res.send({
-        data: 'dolapo',
-        email: 'claceey@gmail.com'
-    })
-
-    // const browser = await puppeteer.launch({headless: 'new'});
-    // const page = await browser.newPage()
-
-    // try {
-    //     // Navigate the page to a URL
-    //     await page.goto('https://example.com/');
-
-    //     const title = await page.$$eval('p', (doc) => {
-    //         const arr: any = []
-    //         doc.forEach(data => arr.push(data.textContent))
-    //         return arr
-    //     })
-    //     res.send({data: title})
-    // }
-    // catch(err: any) {
-    //     console.log(err.message)
-    // }
-    // finally {
-    //     await browser.close();
-    // }
+   const data = await getPage()
+   res.send(data)
 })
 
 app.listen(5000, () => console.log('server started: 5000'))
